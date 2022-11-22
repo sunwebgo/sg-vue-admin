73225a1451be7957445bf777dce1acf406e57f19
“XuHuaiAng” <2533694604@qq.com>
2022-11-22 14:55:58 +0800

    更新route文件夹下的index.js

--- src/router/index.js
+++ src/router/index.js
@@ -46,12 +46,97 @@ export const constantRoutes = [
       component: () => import('@/views/dashboard/index'),
       meta: { title: '首页', icon: 'dashboard' }
     }]
+  },
+  {
+    path: '/write',
+    component: Layout,
+    children: [{
+      path: '/',
+      name: 'Write',
+      component: () => import('@/views/content/article/write/index'),
+      hidden: true
+    }]
+  },
+  {
+    path: '/system/user',
+    component: Layout,
+    children: [{
+      path: '/',
+      name: 'User',
+      component: () => import('@/views/system/user'),
+      hidden: true
+    }]
+  },
+  {
+    path: '/system/role',
+    component: Layout,
+    children: [{
+      path: '/',
+      name: 'role',
+      component: () => import('@/views/system/role'),
+      hidden: true
+    }]
+  },
+  {
+    path: '/system/menu',
+    component: Layout,
+    children: [{
+      path: '/',
+      name: 'menu',
+      component: () => import('@/views/system/menu'),
+      hidden: true
+    }]
+  },
+  {
+    path: '/system/role',
+    component: Layout,
+    children: [{
+      path: '/',
+      name: 'role',
+      component: () => import('@/views/system/role'),
+      hidden: true
+    }]
+  },
+  {
+    path: '/content/article',
+    component: Layout,
+    children: [{
+      path: '/',
+      name: 'article',
+      component: () => import('@/views/content/article/index'),
+      hidden: true
+    }]
+  },
+  {
+    path: '/content/category',
+    component: Layout,
+    children: [{
+      path: '/',
+      name: 'category',
+      component: () => import('@/views/content/category/index'),
+      hidden: true
+    }]
+  },
+  {
+    path: '/content/link',
+    component: Layout,
+    children: [{
+      path: '/',
+      name: 'link',
+      component: () => import('@/views/content/link/index'),
+      hidden: true
+    }]
+  },
+  {
+    path: '/content/tag',
+    component: Layout,
+    children: [{
+      path: '/',
+      name: 'tag',
+      component: () => import('@/views/content/tag/index'),
+      hidden: true
+    }]
   }
-  // {
-  //   path: '/write',
-  //   component: () => import('@/views/content/article/write/index'),
-  //   hidden: true
-  // }
 ]
 
 const createRouter = () => new Router({
