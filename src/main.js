9af9250df5fa8a729dfc8cb41926715d337ed9e1
xuhuaiang <xuhuaiang2000@163.com>
2023-05-13 10:12:38 +0800

    update: 修复低版本Node.js启动问题

--- src/main.js
+++ src/main.js
@@ -4,7 +4,6 @@ import 'normalize.css/normalize.css' // A modern alternative to CSS resets
 
 import ElementUI from 'element-ui'
 import 'element-ui/lib/theme-chalk/index.css'
-import locale from 'element-ui/lib/locale/lang/en' // lang i18n
 import directive from './directive' // directive
 import '@/styles/index.scss' // global css
 
